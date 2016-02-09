9592359dabf608ac0983bcfe7f3127291fcaba03
Asael Jaimes <realnerdo@gmail.com>
2016-02-09 17:03:49 -0600

    New layout & styles

---
+++ resources/views/themes/realnerdo/partials/article.blade.php
@@ -0,0 +1,22 @@
+{{-- Article --}}
+<article class="article">
+    {{-- Cover --}}
+    <div class="cover" style="background: url() no-repeat center center; background-size: cover;"></div>
+    {{-- Date --}}
+    <div class="date">Febrero 14, 2016 · 9 Comentarios</div>
+    {{-- Title --}}
+    <h1 class="title">NOT ALL BLANK CASSETTES ARE CREATED EQUAL</h1>
+    {{-- Body --}}
+    <div class="body">
+        <p>
+            Yeah, yeah what are you wearing, Dave. I'd like you to meet my good friend George McFly. Yeah, I'll keep that in mind. Just relax now Calvin, you've got a big bruise on you're head. Marty, that's completely out of the question, you must not leave this house. you must not see anybody or talk to anybody. Anything you do could have serious reprocautions on future events. Do you understand?
+        </p>
+        <p>
+            <img src="http://unsplash.it/700/350/?random" alt="unsplash" />
+        </p>
+        <p>
+            Yeah, yeah what are you wearing, Dave. I'd like you to meet my good friend George McFly. Yeah, I'll keep that in mind. Just relax now Calvin, you've got a big bruise on you're head. Marty, that's completely out of the question, you must not leave this house. you must not see anybody or talk to anybody. Anything you do could have serious reprocautions on future events. Do you understand?
+        </p>
+    </div>
+
+</article>
--- resources/views/themes/realnerdo/partials/stream.blade.php
+++ resources/views/themes/realnerdo/partials/stream.blade.php
@@ -1,10 +1,40 @@
-{{-- Articles --}}
-<section class="articles">
+{{-- Stream --}}
+<section class="stream">
     {{-- Article --}}
-    <article class="article">
+    <article class="article row">
         {{-- Cover --}}
         <div class="cover col-5">
-            <img src="https://unsplash.it/220/150/?random" alt="Article Title" class="img">
+            <img src="https://unsplash.it/320/220/?random" alt="Article Title" class="img">
+        </div>
+        {{-- /Cover --}}
+        {{-- Content --}}
+        <div class="content col-7">
+            {{-- Date --}}
+            <div class="date">Febrero 14, 2016</div>
+            {{-- /Date --}}
+            {{-- Title --}}
+            <h2 class="title">
+                <a href="#" class="link">Microsoft Patch Management For Home Users</a>
+            </h2>
+            {{-- /Title --}}
+            {{-- Excerpt --}}
+            <div class="excerpt">
+                <p>Marty, I always wear a suit to the office. You alright? Huh? C'mon, he's not that bad. At least he's letting you borrow the car tomorrow night.</p>
+            </div>
+            {{-- /Excerpt --}}
+            {{-- Read --}}
+            <a href="#" class="read">Leer</a>
+            {{-- /Read --}}
+        </div>
+        {{-- /Content --}}
+    </article>
+    {{-- /Article --}}
+
+    {{-- Article --}}
+    <article class="article row">
+        {{-- Cover --}}
+        <div class="cover col-5">
+            <img src="https://unsplash.it/320/220/?random" alt="Article Title" class="img">
         </div>
         {{-- /Cover --}}
         {{-- Content --}}
@@ -31,10 +61,10 @@
     {{-- /Article --}}
 
     {{-- Article --}}
-    <article class="article">
+    <article class="article row">
         {{-- Cover --}}
         <div class="cover col-5">
-            <img src="https://unsplash.it/220/150/?random" alt="Article Title" class="img">
+            <img src="https://unsplash.it/320/220/?random" alt="Article Title" class="img">
         </div>
         {{-- /Cover --}}
         {{-- Content --}}
@@ -61,10 +91,10 @@
     {{-- /Article --}}
 
     {{-- Article --}}
-    <article class="article">
+    <article class="article row">
         {{-- Cover --}}
         <div class="cover col-5">
-            <img src="https://unsplash.it/220/150/?random" alt="Article Title" class="img">
+            <img src="https://unsplash.it/320/220/?random" alt="Article Title" class="img">
         </div>
         {{-- /Cover --}}
         {{-- Content --}}
@@ -91,130 +121,10 @@
     {{-- /Article --}}
 
     {{-- Article --}}
-    <article class="article">
+    <article class="article row">
         {{-- Cover --}}
         <div class="cover col-5">
-            <img src="https://unsplash.it/220/150/?random" alt="Article Title" class="img">
-        </div>
-        {{-- /Cover --}}
-        {{-- Content --}}
-        <div class="content col-7">
-            {{-- Date --}}
-            <div class="date">Febrero 14, 2016</div>
-            {{-- /Date --}}
-            {{-- Title --}}
-            <h2 class="title">
-                <a href="#" class="link">Microsoft Patch Management For Home Users</a>
-            </h2>
-            {{-- /Title --}}
-            {{-- Excerpt --}}
-            <div class="excerpt">
-                <p>Wait a minute. C'mon. This sounds pretty heavy. Save the clock tower. Yes, definitely.</p>
-            </div>
-            {{-- /Excerpt --}}
-            {{-- Read --}}
-            <a href="#" class="read">Leer</a>
-            {{-- /Read --}}
-        </div>
-        {{-- /Content --}}
-    </article>
-    {{-- /Article --}}
-
-    {{-- Article --}}
-    <article class="article">
-        {{-- Cover --}}
-        <div class="cover col-5">
-            <img src="https://unsplash.it/220/150/?random" alt="Article Title" class="img">
-        </div>
-        {{-- /Cover --}}
-        {{-- Content --}}
-        <div class="content col-7">
-            {{-- Date --}}
-            <div class="date">Febrero 14, 2016</div>
-            {{-- /Date --}}
-            {{-- Title --}}
-            <h2 class="title">
-                <a href="#" class="link">Microsoft Patch Management For Home Users</a>
-            </h2>
-            {{-- /Title --}}
-            {{-- Excerpt --}}
-            <div class="excerpt">
-                <p>Wait a minute. C'mon. This sounds pretty heavy. Save the clock tower. Yes, definitely.</p>
-            </div>
-            {{-- /Excerpt --}}
-            {{-- Read --}}
-            <a href="#" class="read">Leer</a>
-            {{-- /Read --}}
-        </div>
-        {{-- /Content --}}
-    </article>
-    {{-- /Article --}}
-
-    {{-- Article --}}
-    <article class="article">
-        {{-- Cover --}}
-        <div class="cover col-5">
-            <img src="https://unsplash.it/220/150/?random" alt="Article Title" class="img">
-        </div>
-        {{-- /Cover --}}
-        {{-- Content --}}
-        <div class="content col-7">
-            {{-- Date --}}
-            <div class="date">Febrero 14, 2016</div>
-            {{-- /Date --}}
-            {{-- Title --}}
-            <h2 class="title">
-                <a href="#" class="link">Microsoft Patch Management For Home Users</a>
-            </h2>
-            {{-- /Title --}}
-            {{-- Excerpt --}}
-            <div class="excerpt">
-                <p>Wait a minute. C'mon. This sounds pretty heavy. Save the clock tower. Yes, definitely.</p>
-            </div>
-            {{-- /Excerpt --}}
-            {{-- Read --}}
-            <a href="#" class="read">Leer</a>
-            {{-- /Read --}}
-        </div>
-        {{-- /Content --}}
-    </article>
-    {{-- /Article --}}
-
-    {{-- Article --}}
-    <article class="article">
-        {{-- Cover --}}
-        <div class="cover col-5">
-            <img src="https://unsplash.it/220/150/?random" alt="Article Title" class="img">
-        </div>
-        {{-- /Cover --}}
-        {{-- Content --}}
-        <div class="content col-7">
-            {{-- Date --}}
-            <div class="date">Febrero 14, 2016</div>
-            {{-- /Date --}}
-            {{-- Title --}}
-            <h2 class="title">
-                <a href="#" class="link">Microsoft Patch Management For Home Users</a>
-            </h2>
-            {{-- /Title --}}
-            {{-- Excerpt --}}
-            <div class="excerpt">
-                <p>Wait a minute. C'mon. This sounds pretty heavy. Save the clock tower. Yes, definitely.</p>
-            </div>
-            {{-- /Excerpt --}}
-            {{-- Read --}}
-            <a href="#" class="read">Leer</a>
-            {{-- /Read --}}
-        </div>
-        {{-- /Content --}}
-    </article>
-    {{-- /Article --}}
-
-    {{-- Article --}}
-    <article class="article">
-        {{-- Cover --}}
-        <div class="cover col-5">
-            <img src="https://unsplash.it/220/150/?random" alt="Article Title" class="img">
+            <img src="https://unsplash.it/320/220/?random" alt="Article Title" class="img">
         </div>
         {{-- /Cover --}}
         {{-- Content --}}
