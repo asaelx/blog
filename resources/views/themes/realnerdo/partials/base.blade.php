d8084a3e86b557d94e4b5c88850f06142cebdb02
Asael Jaimes <realnerdo@gmail.com>
2016-02-09 02:28:13 -0600

    Home basic markup & styling

--- resources/views/themes/realnerdo/partials/base.blade.php
+++ resources/views/themes/realnerdo/partials/base.blade.php
@@ -2,9 +2,13 @@
 <html lang="es">
     <head>
         <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
         <title>@yield('title')</title>
+        <link rel="stylesheet" href="{{ asset('css/themes/realnerdo/master.css') }}" media="screen" title="no title" charset="utf-8">
     </head>
     <body>
+        @include('themes.realnerdo.partials.topbar')
         @yield('content')
+        <script type="text/javascript" src="{{ asset('js/themes/realnerdo/magic.js') }}"></script>
     </body>
 </html>
---
+++ resources/views/themes/realnerdo/partials/instagram.blade.php
@@ -0,0 +1,65 @@
+<div class="instagram">
+    <h3 class="title">Instagram</h3>
+    <ul id="feed">
+        <li class="item">
+            <a href="#" class="link">
+                <img src="http://unsplash.it/60/60/?random" alt="" class="photo">
+            </a>
+        </li>
+        <li class="item">
+            <a href="#" class="link">
+                <img src="http://unsplash.it/60/60/?random" alt="" class="photo">
+            </a>
+        </li>
+        <li class="item">
+            <a href="#" class="link">
+                <img src="http://unsplash.it/60/60/?random" alt="" class="photo">
+            </a>
+        </li>
+        <li class="item">
+            <a href="#" class="link">
+                <img src="http://unsplash.it/60/60/?random" alt="" class="photo">
+            </a>
+        </li>
+        <li class="item">
+            <a href="#" class="link">
+                <img src="http://unsplash.it/60/60/?random" alt="" class="photo">
+            </a>
+        </li>
+        <li class="item">
+            <a href="#" class="link">
+                <img src="http://unsplash.it/60/60/?random" alt="" class="photo">
+            </a>
+        </li>
+        <li class="item">
+            <a href="#" class="link">
+                <img src="http://unsplash.it/60/60/?random" alt="" class="photo">
+            </a>
+        </li>
+        <li class="item">
+            <a href="#" class="link">
+                <img src="http://unsplash.it/60/60/?random" alt="" class="photo">
+            </a>
+        </li>
+        <li class="item">
+            <a href="#" class="link">
+                <img src="http://unsplash.it/60/60/?random" alt="" class="photo">
+            </a>
+        </li>
+        <li class="item">
+            <a href="#" class="link">
+                <img src="http://unsplash.it/60/60/?random" alt="" class="photo">
+            </a>
+        </li>
+        <li class="item">
+            <a href="#" class="link">
+                <img src="http://unsplash.it/60/60/?random" alt="" class="photo">
+            </a>
+        </li>
+        <li class="item">
+            <a href="#" class="link">
+                <img src="http://unsplash.it/60/60/?random" alt="" class="photo">
+            </a>
+        </li>
+    </ul>
+</div>
